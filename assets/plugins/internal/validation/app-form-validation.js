0815822d061adc20bcaf719c0d3dca3bcc2115aa
Ziron Media <dev@zironmedia.com>
2026-04-04 13:37:35 +0400

    chore: update URLs in configuration and error messages to reflect new domain

--- assets/plugins/internal/validation/app-form-validation.js
+++ assets/plugins/internal/validation/app-form-validation.js
@@ -1,7 +1,7 @@
 /*!
  * Internal App Plugin for validation that extends jQuery Validation plugin.
  *
- * https://perfexcrm.com/
+ * https://www.zironpro.ae/
  *
  * Copyright (c) 2019 Marjan Stojanov
  */
